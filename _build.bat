265577ade8f627b4f4de80ead743246b08cc13ca
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2024-03-16 22:58:51 -0400

    replace clownnemesis

--- _build.bat
+++ _build.bat
@@ -1,8 +1,8 @@
 @echo off
 
-clownnemesis -c SonLVL\chunks.unc SonLVL\chunks.nem
-clownnemesis -c SonLVL\blocks.unc SonLVL\blocks.nem
-clownnemesis -c SonLVL\index.unc SonLVL\index.nem
+nemcmp SonLVL\chunks.unc SonLVL\chunks.nem
+nemcmp SonLVL\blocks.unc SonLVL\blocks.nem
+nemcmp SonLVL\index.unc SonLVL\index.nem
 
 IF EXIST "RIBUILT.BIN" move /Y "RIBUILT.BIN" "RIBUILT.PREV.BIN" >NUL
 asm68k /k /p /o ae- Compile.asm, "RIBUILT.BIN" >errors.txt, chaotx.sym, chaotx.lst
